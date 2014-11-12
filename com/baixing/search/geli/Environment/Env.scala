dec8cba814515115a50ba0e35960d9dbdc19d1ae
Sunyifan <sunyifan@baixing.com>
2014-11-12 10:46:10 +0800

    [update] Env属性是private

--- com/baixing/search/geli/Environment/Env.scala
+++ com/baixing/search/geli/Environment/Env.scala
@@ -8,8 +8,8 @@ import org.apache.spark.{SparkConf, SparkContext}
  * Created by abzyme-baixing on 14-11-12.
  */
 class Env (conf : Configuration){
-	val sc : SparkContext = new SparkContext(new SparkConf().setAppName("digword_" + conf.toString()))
-	val hc : HiveContext = new HiveContext(sc)
+	private val sc : SparkContext = new SparkContext(new SparkConf().setAppName("digword_" + conf.toString()))
+	private val hc : HiveContext = new HiveContext(sc)
 	def sparkContext() : SparkContext = sc
 	def hiveContext() : HiveContext = hc
 }
