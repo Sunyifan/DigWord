5b3b123164e41ff7ddf8930787507534054cef91
Sunyifan <sunyifan@baixing.com>
2014-11-12 11:02:47 +0800

    [update] 添加数据工具类，移植拿帖子函数

---
+++ com/baixing/search/geli/Util/DataUtil.scala
@@ -0,0 +1,34 @@
+package com.baixing.search.geli.Util
+
+import com.baixing.search.geli.Configuration.Configuration
+import com.baixing.search.geli.Environment.Env
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SchemaRDD
+
+
+/**
+ * Created by abzyme-baixing on 14-11-12.
+ */
+object DataUtil {
+	def AdInputRDD(conf : Configuration, env : Env) : RDD[(String,String)] = {
+		getAd(conf, env).map{ row => (row(0).toString, row(1).toString + " " + row(2).toString)}
+	}
+
+	private def getAd (conf : Configuration, env : Env) : SchemaRDD = {
+		env.hiveContext().hql(buildAdQuery(conf))
+	}
+
+	private def buildAdQuery (conf : Configuration) : String = {
+		val category = conf.category
+		val areaid = conf.areaId
+		val fromdate = conf.fromdate
+		val todate = conf.todate
+
+		" SELECT ad_id, title, content"  +
+			" FROM shots.ad_content " +
+			" WHERE category = '" + category + "' and " +
+			"area_id = '" + areaid + "' and " +
+			"dt < '" + todate + "' and " +
+			"dt >= '" + fromdate + "'"
+	}
+}
