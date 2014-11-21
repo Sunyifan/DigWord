25b9ad8cc60d0314948b1689d12907f2ca37fc77
Sunyifan <sunyifan@baixing.com>
2014-11-21 17:35:59 +0800

    [update] 重构文本处理函数

--- com/baixing/search/geli/Util/Text.scala
+++ com/baixing/search/geli/Util/Text.scala
@@ -6,69 +6,54 @@ import scala.collection.mutable.ArrayBuffer
  * Created by abzyme-baixing on 14-11-12.
  */
 object Text {
+	private val reserveChar = Array('*', '-', 'X', '.','\\')
+	private val stopString = Array("\\r", "\\n")
+
+	private def isValidChar(c : Character): Boolean ={
+		Character.isAlphabetic(c.toInt) || Character.isDigit(c) || reserveChar.contains(c)
+	}
+
+	private def removeStopString(str : String) : Array[String] = {
+		var ret = str
+		for (s <- stopString){
+			ret = ret.replace(s, " ")
+		}
+
+		ret.split(" ").filter(_.length != 0)
+	}
+
 	def preproccess(content: String): ArrayBuffer[String] = {
 		val ret = ArrayBuffer[String]()
-		val puncs = Array(',', '，', '.', '。', '!', '！', '?', '？', ';', '；', ':', '：', '\'', '‘', '’', '\"', '”', '“', '、', '(', '（', ')', '）', '<', '《', '>', '》', '[', '【', '】', ']', '{', '}', ' ', '\t', '\r', '\n') // 标点集合
-		var tmp = ""
-		var i = 0
-		var before = ' '
-		for (ch <- content) {
-			i += 1
-			if (ch == '.' && Character.isDigit(before) && i < (content.length) && Character.isDigit(content.charAt(i))) {
-				tmp += ch
-			}
-			else if (puncs.contains(ch)) {
-				if (tmp != "") {
-					ret ++= dealWithSpecialCase(tmp)
-					tmp = ""
-				}
-			}
-			else {
-				if (isMeaningful(ch)) tmp += ch
-				if (i == content.length) {
-					ret ++= dealWithSpecialCase(tmp)
-					tmp = ""
-				}
-			}
-			before = ch
-		}
-		ret
-	}
 
-	def dealWithSpecialCase(content: String): ArrayBuffer[String] = {
-		val patterns = Array("\\\\r\\\\n", "\\\\n", "\\\\t", "[0-9]{8,}");
-		val tmp = ArrayBuffer[String]()
-		val ret = ArrayBuffer[String]()
-		ret += content
-		for (pat <- patterns) {
-			tmp.clear()
-			for (ele <- ret) {
-				val e = ele.trim()
-				if (e != "") {
-					tmp ++= e.replaceAll(pat, "|").split( """\|""")
-				}
-			}
-			ret.clear()
-			ret ++= tmp.clone()
-		}
-		ret
-	}
+		var start, end = 0
 
-	def isMeaningful(ch: Char): Boolean = {
-		var ret = false
-		val meaningfulMarks = Array('*', '-', 'X', '.','\\')
-		if ((ch >= '一' && ch <= '龥') || (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9') || meaningfulMarks.contains(ch))
-			ret = true
+		while(start != content.length){
+			end = start
+			val buf = new StringBuilder
+
+			while( end != content.length && isValidChar(content(end))){
+				buf.append(content(end))
+				end += 1
+			}
+
+			if (buf.length != 0)
+				ret ++= removeStopString(buf.toString)
+
+			if (start == end)
+				end += 1
+
+			start = end
+		}
+
 		ret
 	}
 
 	def splitWord(v: String, wordLength: Int ):ArrayBuffer[String] = {
 		val len = v.length
-		//textLength += len
+
 		val greetStrings =  ArrayBuffer[String]()
 		for (i <- 0 to len - 1) {
-			// 单词起始点位置
-			var j: Int = 1 // 新词长度
+			var j: Int = 1
 			while (i + j <= len && j <= wordLength) {
 				val tmp: String = v.substring(i, i + j)
 				greetStrings += tmp
@@ -77,7 +62,6 @@ object Text {
 		}
 		greetStrings
 	}
-
 
 	def binSearch(arr : Array[String], item : String): Int ={
 		java.util.Arrays.binarySearch(arr.asInstanceOf[Array[AnyRef]], item)
