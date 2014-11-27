774753837f15ec8e3ceafc871d4b548e442b5d67
Sunyifan <sunyifan@baixing.com>
2014-11-27 21:52:41 +0800

    [update] 调整一下Env的结构，舍弃了Configuration，因为所有conf都能放在SparkConf里

--- com/baixing/search/geli/Environment/Env.scala
+++ com/baixing/search/geli/Environment/Env.scala
@@ -1,15 +1,48 @@
 package com.baixing.search.geli.Environment
 
-import com.baixing.search.geli.Configuration.Configuration
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
  * Created by abzyme-baixing on 14-11-12.
  */
-class Env (conf : Configuration){
-	private val sc : SparkContext = new SparkContext(new SparkConf().setAppName("digword_" + conf.toString()))
-	private val hc : HiveContext = new HiveContext(sc)
-	def sparkContext() : SparkContext = sc
-	def hiveContext() : HiveContext = hc
+object Env {
+	private var sc : SparkContext = null
+	private var hc : HiveContext = null
+	private val conf : SparkConf = new SparkConf()
+
+	def sparkContext() : SparkContext = {
+		if (sc == null)
+			sc = new SparkContext(conf)
+		sc
+	}
+
+	def hiveContext() : HiveContext = {
+		if (hc == null)
+			hc = new HiveContext(sc)
+		hc
+	}
+
+	private def set(k : String, v : String) = conf.set(k, v)
+	def setMaster(master : String) = conf.setMaster(master)
+	def setAppName(appName : String) = conf.setAppName(appName)
+	def setJar(jar : String) = conf.setJars(Array(jar))
+	def setExecutorMemoery(mem : String) = conf.set("spark.executor.memory", mem)
+
+	def getProperty(k : String) : String = {
+		conf.get(k)
+	}
+
+	def init(args : Array[String]): Unit = {
+		set("area_id", args(0))
+		set("category", args(1))
+		set("fromdate", args(2))
+		set("todate", args(3))
+		setAppName(args(4))
+		setMaster(args(5))
+		setJar(args(6))
+		setExecutorMemoery(args(7))
+		sparkContext()
+	}
 }
+
--- com/baixing/search/geli/Util/Data.scala
+++ com/baixing/search/geli/Util/Data.scala
@@ -1,63 +1,58 @@
 package com.baixing.search.geli.Util
 
-import com.baixing.search.geli.Configuration.Configuration
 import com.baixing.search.geli.Environment.Env
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SchemaRDD
 
 /**
- * Created by abzyme-baixing on 14-11-12.
+ * Created by abzyme-baixing on 14-11-27.
  */
 object Data {
-	// get ad
-	def adContentWithId(conf : Configuration, env : Env) : RDD[(String, String, String)] ={
-		RawAd(conf, env).map{ row => (row(0).toString, row(1).toString + " " + row(2).toString, row(3).toString)}
+	// ad interface
+	def adContentWithId() : RDD[(String, String, String)] ={
+		RawAd().map{ row => (row(0).toString, row(1).toString + " " + row(2).toString, row(3).toString)}
 	}
 
-	def adContent(conf : Configuration, env : Env) : RDD[String] = {
-		RawAd(conf, env).map{ row => row(1).toString + " " + row(2).toString}
+	def adContent() : RDD[String] = {
+		RawAd().map{ row => (row(1).toString + " " + row(2).toString)}
 	}
 
-	def adTag(conf : Configuration, env : Env): RDD[(String, String)] = {
-		RawAd(conf, env).map{ row => (row(0).toString, row(3).toString)}
+	private def RawAd () : SchemaRDD = {
+		Env.hiveContext().sql(AdQuery())
 	}
 
-	private def RawAd (conf : Configuration, env : Env) : SchemaRDD = {
-		env.hiveContext().hql(AdQuery(conf))
-	}
-
-	private def AdQuery (conf : Configuration) : String = {
-		val category = conf.category
-		val areaid = conf.areaId
-		val fromdate = conf.fromdate
-		val todate = conf.todate
+	private def AdQuery () : String = {
+		val category = Env.getProperty("category")
+		val areaid = Env.getProperty("area_id")
+		val fromdate = Env.getProperty("fromdate")
+		val todate = Env.getProperty("todate")
 
 		"\nSELECT\n" +
-		"    ad_id,\n " +
-		"    title,\n" +
-		"    content,\n"  +
-		"    tags\n" +
-		"FROM\n" +
-		"    logs.ad_content\n" +
-		"WHERE\n" +
-		"    category = '" + category + "'\n" +
-		"    and area_id = '" + areaid + "'\n" +
-		"    and dt between " + fromdate + " and " + todate + "\n" +
-		"    and tags is not null"
+			"    ad_id,\n " +
+			"    title,\n" +
+			"    content,\n"  +
+			"    tags\n" +
+			"FROM\n" +
+			"    logs.ad_content\n" +
+			"WHERE\n" +
+			"    category = '" + category + "'\n" +
+			"    and area_id = '" + areaid + "'\n" +
+			"    and dt between " + fromdate + " and " + todate + "\n" +
+			"    and tags is not null"
 	}
 
 
-	// get user action data
-	def adVisitor(conf : Configuration, env : Env): RDD[(String, String)] ={
-		RawUserAction(conf, env).filter{
+	// user action interface
+	def adVisitor(): RDD[(String, String)] ={
+		RawUserAction().filter{
 			row => row(2).toString != "0"
 		}.map{
 			row => (row(2).toString, row(0).toString)
 		}
 	}
 
-	def query(conf : Configuration, env : Env): RDD[String] = {
-		RawUserAction(conf, env).filter{
+	def query(): RDD[String] = {
+		RawUserAction().filter{
 			row => row(1) != null && row(1).toString.indexOf("query=") >= 0
 		}.map{
 			row =>
@@ -65,36 +60,47 @@ object Data {
 		}.filter(_.length > 0)
 	}
 
-	def RawUserAction(conf : Configuration, env : Env) : SchemaRDD = {
-		env.hiveContext().hql(UserActionQuery(conf))
+	def UserActionBeforeVad(): RDD[(String, String, String)] ={
+		RawUserAction().filter{ row => row(2).toString != "0" && row(1).toString.indexOf("query=") >= 0}
+			.map{ row =>
+			val visitor_id = row(0).toString
+			val query = row(1).toString.split(",").filter(_.startsWith("query="))(0).substring(6)
+			val ad_id = row(2).toString
+			(visitor_id, query, ad_id)}.filter(_._2.length != 0)
 	}
 
-	def UserActionQuery(conf : Configuration) : String = {
-		val category = conf.category
-		val areaid = conf.areaId
-		val fromdate = conf.fromdate
-		val todate = conf.todate
+	def RawUserAction() : SchemaRDD = {
+		Env.hiveContext().sql(UserActionQuery())
+	}
+
+	def UserActionQuery() : String = {
+		val category = Env.getProperty("category")
+		val areaid = Env.getProperty("area_id")
+		val fromdate = Env.getProperty("fromdate")
+		val todate = Env.getProperty("todate")
 
 
 		"\nSELECT\n" +
-		"    visitor_id,\n" +
-		"    referer['query'],\n" +
-		"    landing['ad_id']\n" +
-		"FROM\n" +
-		"    base.user_actions\n" +
-		"WHERE\n" +
-		"    dt between " + fromdate + " and " + todate + "\n" +
-		"    and referer['url'] not like '%select%'\n" +
-		"    and landing['city_id'] = '" + areaid + "'\n" +
-		"    and landing['category_name_en'] = '" + category + "'\n" +
-		"    and landing['url_type'] = 4\n" +
-		"    and (platform = 'wap' or platform = 'web')\n"
+			"    visitor_id,\n" +
+			"    referer['query'],\n" +
+			"    landing['ad_id']\n" +
+			"FROM\n" +
+			"    base.user_actions\n" +
+			"WHERE\n" +
+			"    dt between " + fromdate + " and " + todate + "\n" +
+			"    and referer['url'] not like '%select%'\n" +
+			"    and landing['city_id'] = '" + areaid + "'\n" +
+			"    and landing['category_name_en'] = '" + category + "'\n" +
+			"    and landing['url_type'] = 4\n" +
+			"    and (platform = 'wap' or platform = 'web')\n"
 	}
 
-	// get geli
+
+	// temp interface for geli
 	private val root = "/user/sunyifan/geli/"
-	def geli(conf : Configuration, env : Env): RDD[String] ={
-		val geliPath = root + conf.category() + "/" + conf.fromdate + "-" + conf.todate + "-" + conf.areaId
-		env.sparkContext().textFile(geliPath)
+	def geli() : RDD[String] = {
+		val geliPath = root + Env.getProperty("category") + "/" + Env.getProperty("fromdate") + "-" +
+													Env.getProperty("todate") + "-" + Env.getProperty("area_id")
+		Env.sparkContext().textFile(geliPath)
 	}
 }
