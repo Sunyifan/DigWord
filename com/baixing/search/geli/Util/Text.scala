729078bceee4032af686c1c43aa481dba745ec63
Sunyifan <sunyifan@baixing.com>
2014-12-23 11:49:36 +0800

    [update]改一下预处理

--- com/baixing/search/geli/Util/Text.scala
+++ com/baixing/search/geli/Util/Text.scala
@@ -1,21 +1,16 @@
 package com.baixing.search.geli.Util
 
-import java.io.StringReader
-
-import org.wltea.analyzer.core.{Lexeme, IKSegmenter}
-
 import scala.collection.mutable.ArrayBuffer
-import scala.collection.mutable.Map
-import scala.util.control.Breaks
 
 /**
  * Created by abzyme-baixing on 14-11-12.
  */
 object Text {
-	private val stopString = Array("\\r", "\\n", "\\\r", "\\\n")
+	private val stopString = Array("\\r", "\\n")
+	private val reservedString = Array("\\", ".", "/", "㎡")
 
 	private def isValidChar(c : Character): Boolean ={
-		Character.isAlphabetic(c.toInt) || Character.isDigit(c)
+		Character.isAlphabetic(c.toInt) || Character.isDigit(c) || reservedString.contains(c.toString)
 	}
 
 	private def removeStopString(str : String) : Array[String] = {
@@ -28,7 +23,7 @@ object Text {
 	}
 
 	def preproccess(content: String): ArrayBuffer[String] = {
-		val ret = ArrayBuffer[String]()
+		val ret = new ArrayBuffer[String]
 		var start, end = 0
 
 		while(start != content.length){
@@ -65,49 +60,5 @@ object Text {
 			}
 		}
 		greetStrings
-	}
-
-	def find(arr : Array[String], item : String): Int ={
-		java.util.Arrays.binarySearch(arr.asInstanceOf[Array[AnyRef]], item)
-	}
-
-
-	def entrophy(charArray : Array[Char]): Double ={
-		val len = charArray.length.toDouble
-		val charCnt = Map[Char, Int]()
-		var ret : Double = 0.0
-
-		for (c <- charArray){
-			if (!charCnt.contains(c))
-				charCnt += (c -> 0)
-
-			charCnt(c) += 1
-		}
-
-		for((k, v) <- charCnt){
-			ret = ret - v.toDouble / len * Math.log(v.toDouble / len)
-		}
-
-		ret
-	}
-
-	def ikSeg(text : String): StringBuilder ={
-		val reader = new StringReader(text)
-		val analyzer = new IKSegmenter(reader, true)
-
-		var t : Lexeme = null
-		val loop = new Breaks
-		val sb = new StringBuilder
-
-		loop.breakable{
-			while(true){
-				t = analyzer.next()
-				if (t == null)
-					loop.break()
-				sb ++= t.getLexemeText
-			}
-		}
-
-		sb
 	}
 }
