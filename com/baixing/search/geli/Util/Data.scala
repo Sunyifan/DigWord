53aee0f008356756ab65f91ae0ddc8c43b1de8e8
Sunyifan <sunyifan@baixing.com>
2015-01-04 20:34:56 +0800

    [update]数据接口

--- com/baixing/search/geli/Util/Data.scala
+++ com/baixing/search/geli/Util/Data.scala
@@ -1,12 +1,23 @@
 package com.baixing.search.geli.Util
 
 import com.baixing.search.geli.Environment.Env
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SchemaRDD
 
 /**
  * Created by abzyme-baixing on 14-11-27.
  */
 object Data {
+	def adContent() : RDD[String] ={
+		DataSource.getRaw("logs.ad_content", Array("title", "content", "area_id", "category", "dt"))
+					.filter(row => row(2).toString == Env.sparkConf().get("area_id"))
+						.filter(row => row(3).toString == Env.sparkConf().get("category"))
+							.filter(row => row(4).toString >= Env.sparkConf().get("fromdate") && row(5).toString <= Env.sparkConf().get("todate"))
+								.map(row => row(0).toString + " " + row(1).toString)
+	}
+}
+
+object DataSource{
 	def getRaw(tableName : String, fields : Array[String]): SchemaRDD ={
 		val hc = Env.hiveContext()
 		import hc._
@@ -14,4 +25,3 @@ object Data {
 		hc.table(tableName).select(fields.map(field => symbolToUnresolvedAttribute(Symbol(field))):_*)
 	}
 }
-
